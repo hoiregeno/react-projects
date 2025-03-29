c9f4f1d08af1dda413f102fe34efdc68384f283f
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 15:30:40 +1000

    Declared state variables for city input, weather data & error message using useState hook.

--- react-todo/src/components/WeatherApp.jsx
+++ react-todo/src/components/WeatherApp.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const WeatherApp = () => {
+    // Declare state variables for location input and weather data.
+    const [location, setLocation] = useState("");
+    const [weather, setWeather] = useState(null);
+    const [errorMessage, setErrorMessage] = useState("");
+
     return (
-        <div>WeatherApp</div>
+        <div className='card'>
+
+        </div>
     )
 }
 
