175637d0ff445479b7300093323bcc09e8db8a39
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 14:12:06 +1000

    Added image elements for the navar.

--- navbar-project/src/components/navbar/Navbar.jsx
+++ navbar-project/src/components/navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <nav className='navbar'>
+            <img src="" alt="" className='logo' />
+
             <ul>
                 <li>Home</li>
                 <li>Products</li>
@@ -14,6 +16,8 @@ const Navbar = () => {
                 <input type="text" placeholder='Search' />
                 <img src="" alt="" />
             </div>
+
+            <img src="" alt="" className='toggle-icon' />
         </nav>
     )
 }
