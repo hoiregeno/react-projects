7dc4ea94a2a3f998d175d8fd3c2886c50cfe6a47
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 15:02:47 +1000

    Used hooks for managing state & side effects.

--- navbar-project/src/App.jsx
+++ navbar-project/src/App.jsx
@@ -1,9 +1,17 @@
+import React, { useEffect, useState } from 'react';
 import Navbar from "./components/navbar/Navbar.jsx";
 
 const App = () => {
+  const currentTheme = localStorage.getItem('current_theme');
+  const [theme, setTheme] = useState(currentTheme ? currentTheme : 'light');
+
+  useEffect(() => {
+    localStorage.setItem('current_theme', theme);
+  }, [theme]);
+
   return (
-    <div className="container">
-      <Navbar />
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
