b839935d43fed9a462ff39726786655fa518621a
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 15:06:20 +1000

    Added Navbar component with theme toggle and search functionality.

--- navbar-project/src/components/navbar/Navbar.jsx
+++ navbar-project/src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import './Navbar.css';
+import logo_light from '../../assets/images/logo-black.png';
+import logo_dark from '../../assets/images/logo-white.png';
+import search_icon_dark from '../../assets/images/search-b.png';
+import search_icon_light from '../../assets/images/search-w.png';
+import toggle_light from '../../assets/images/night.png';
+import toggle_dark from '../../assets/images/day.png';
 
-const Navbar = () => {
+const Navbar = ({ theme, setTheme }) => {
+
+    const toggle_mode = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+    }
+
     return (
         <nav className='navbar'>
-            <img src="" alt="" className='logo' />
+            <img
+                src={theme === 'light' ? logo_light : logo_dark}
+                alt="logo"
+                className='logo'
+            />
 
             <ul>
                 <li>Home</li>
@@ -14,10 +29,18 @@ const Navbar = () => {
 
             <div className="search-box">
                 <input type="text" placeholder='Search' />
-                <img src="" alt="" />
+                <img
+                    src={theme === 'light' ? search_icon_light : search_icon_dark}
+                    alt="search icon"
+                />
             </div>
 
-            <img src="" alt="" className='toggle-icon' />
+            <img
+                onClick={() => toggle_mode()}
+                src={theme === 'light' ? toggle_light : toggle_dark}
+                alt="toggle icon"
+                className='toggle-icon'
+            />
         </nav>
     )
 }
