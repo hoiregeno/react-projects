602cf7bc1699df88bea4d49218a19582a10137cd
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 20:25:38 +1000

    Changed width & height of search button, rounded temp to 2dp and added class name to weather icon.

--- react-todo/src/components/WeatherApp.jsx
+++ react-todo/src/components/WeatherApp.jsx
@@ -73,7 +73,7 @@ const WeatherApp = () => {
                     className='city-input'
                 />
                 <button type="submit" className='search-button'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
                         <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
                     </svg>
                 </button>
@@ -91,10 +91,10 @@ const WeatherApp = () => {
                         {weatherData.name}, {weatherData.sys.country}
                     </h1>
                     <h2 className="temp-display">
-                        {(weatherData.main.temp - 273.15).toFixed(2)}°C
+                        {(weatherData.main.temp - 273.15).toFixed(1)}°C
                     </h2>
                     <p className="feels-display">
-                        {(weatherData.main.feels_like - 273.15).toFixed(2)}°C
+                        Feels like: {(weatherData.main.feels_like - 273.15).toFixed(1)}°C
                     </p>
                     <p className="humidity-display">
                         Humidity: {weatherData.main.humidity}%
@@ -102,7 +102,11 @@ const WeatherApp = () => {
                     <p className="description-display">
                         {weatherData.weather[0].description}
                     </p>
-                    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} alt="weather icon" />
+                    <img
+                        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+                        alt="weather icon"
+                        className='weather-icon'
+                    />
                 </div>
             )}
         </div>
