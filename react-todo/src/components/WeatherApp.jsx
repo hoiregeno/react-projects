c3a1cb936133f6c2d8c7d884a2b358b5a53a5dd9
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 17:34:50 +1000

    Added search SVG icon for the search button & gave the input a class name.

--- react-todo/src/components/WeatherApp.jsx
+++ react-todo/src/components/WeatherApp.jsx
@@ -50,9 +50,12 @@ const WeatherApp = () => {
                     placeholder='Enter city'
                     value={location}
                     onChange={e => setLocation(e.target.value)}
+                    className='city-input'
                 />
-                <button type="submit">
-                    Find
+                <button type="submit" className='search-button'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+                    </svg>
                 </button>
             </form>
 
