374c0bfda1986d40774debcddff3241e500abf7f
hoiregeno <hoiregeno75@gmail.com>
2025-03-29 15:54:43 +1000

    Set up WeatherApp component with state, input handling, and basic UI

--- react-todo/src/components/WeatherApp.jsx
+++ react-todo/src/components/WeatherApp.jsx
@@ -3,12 +3,42 @@ import React, { useState } from 'react'
 const WeatherApp = () => {
     // Declare state variables for location input and weather data.
     const [location, setLocation] = useState("");
-    const [weather, setWeather] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
     const [errorMessage, setErrorMessage] = useState("");
 
-    return (
-        <div className='card'>
+    const getWeatherData = async (city) => {
+        if (city) {
+            try {
+                const response = await fetch();
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }
 
+    const handleInputChange = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className='container'>
+            <form className="weather-form" onSubmit={handleInputChange}>
+                <input
+                    type="text"
+                    placeholder='Enter city'
+                    value={location}
+                    onChange={e => setLocation(e.target.value)}
+                />
+                <button type="submit">
+                    Find
+                </button>
+            </form>
+            {weatherData && (
+                <div className="card">
+
+                </div>
+            )}
         </div>
     )
 }
