b72d51d82a590a1345207ef8c1774e90b5ba7d3e
hoiregeno <hoiregeno75@gmail.com>
2025-04-07 12:08:54 +1000

    Refactored Navbar to use React state for sidebar toggle functionality

--- navbar-project/src/component/Navbar.jsx
+++ navbar-project/src/component/Navbar.jsx
@@ -1,23 +1,22 @@
+import { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = ({ navLinks }) => {
-    const openSiderBar = () => {
-        document.getElementById("navbar").classList.add("show");
-    }
+    const [isOpen, setIsOpen] = useState(false);
 
-    const closeSidebar = () => {
-        document.getElementById("navbar").classList.remove("show");
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
     }
 
     return (
         <>
-            <button className="open-btn" onClick={openSiderBar}>
+            <button className="open-btn" onClick={toggleSidebar}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
                 </svg>
             </button>
-            <nav id="navbar">
-                <button className="close-btn" onClick={closeSidebar}>
+            <nav id="navbar" className={isOpen ? "show" : ""}>
+                <button className="close-btn" onClick={toggleSidebar}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
                     </svg>
@@ -27,7 +26,9 @@ const Navbar = ({ navLinks }) => {
                     {
                         navLinks.map(navLink => (
                             <li key={navLink.name}>
-                                <a href={navLink.url}>
+                                <a
+                                    href={navLink.url}
+                                    onClick={() => setIsOpen(false)}>
                                     {navLink.name}
                                 </a>
                             </li>
