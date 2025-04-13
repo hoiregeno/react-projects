0999d26b52337b36e54bbe6f7e90b9936d4dac6d
hoiregeno <hoiregeno75@gmail.com>
2025-04-14 09:46:49 +1000

    Refactor Navbar component to accept navLinks as props and render navigation items

--- navbar-practical/src/components/Navbar.jsx
+++ navbar-practical/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 
-function Navbar() {
+function Navbar({ navLinks }) {
+    // State to manage the open/close state of the navbar.
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div>Navbar</div>
-    )
+        <nav>
+            <ul className='links-container'>
+                {navLinks.map((link) => (
+                    <li key={link.id}><a href={link.url}>{link.name}</a></li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar
