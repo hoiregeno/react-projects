9ff6b29111d9c73067591dcbc5d0a07e2cb620ee
hoiregeno <hoiregeno75@gmail.com>
2025-04-14 09:36:54 +1000

    Pass navigation links to Navbar component

--- navbar-practical/src/App.jsx
+++ navbar-practical/src/App.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
+  const links = [
+    { name: "Home", url: "#home" },
+    { name: "About", url: "#about" },
+    { name: "Portfolio", url: "#portfolio" },
+    { name: "Contact", url: "#contact" }];
 
   return (
-    <Navbar />
+    <Navbar navLinks={links} />
   );
 }
 
