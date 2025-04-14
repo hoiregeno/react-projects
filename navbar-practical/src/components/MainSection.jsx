744d98501829c6a6aadded9fdb666a008182c1bd
hoiregeno <hoiregeno75@gmail.com>
2025-04-14 13:51:13 +1000

    Add MainSection component with portfolio introduction and contact information

---
+++ navbar-practical/src/components/MainSection.jsx
@@ -0,0 +1,26 @@
+
+function MainSection() {
+
+    return (
+        <main>
+            <section id="home" className="home">
+                <h1>Welcome to My Portfolio</h1>
+                <p>This is a brief introduction about me and my work.</p>
+            </section>
+            <section id="about" className="about">
+                <h2>About Me</h2>
+                <p>Here you can find information about my background and skills.</p>
+            </section>
+            <section id="portfolio" className="portfolio">
+                <h2>My Work</h2>
+                <p>Check out some of my projects and accomplishments.</p>
+            </section>
+            <section id="contact" className="contact">
+                <h2>Contact Me</h2>
+                <p>If you would like to get in touch, please reach out!</p>
+            </section>
+        </main>
+    )
+}
+
+export default MainSection;
