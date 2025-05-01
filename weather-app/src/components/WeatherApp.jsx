ec6e92085559043c1b40b737ff4186c919712d22
hoiregeno <hoiregeno75@gmail.com>
2025-05-01 14:00:16 +1000

    Add alt message attribute to icon for screen readers

--- weather-app/src/components/WeatherApp.jsx
+++ weather-app/src/components/WeatherApp.jsx
@@ -60,7 +60,7 @@ function WeatherApp() {
           <p className="desc-display">{weather.weather[0].description}</p>
           <img
             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-            alt=""
+            alt={`${weather.weather[0].description} icon`}
           />
         </div>
       )}
