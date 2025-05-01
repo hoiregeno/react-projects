1aca31c6220dd5b82ad3e8b0047f7234774cbbea
hoiregeno <hoiregeno75@gmail.com>
2025-05-01 13:17:16 +1000

    Create state variables and set initial states

--- weather-app/src/components/WeatherApp.jsx
+++ weather-app/src/components/WeatherApp.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
 function WeatherApp() {
+  // Create state variables
+  const [cityName, setCityName] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
   return <div>WeatherApp</div>;
 }
 
