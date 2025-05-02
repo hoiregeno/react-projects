37bd5fe3326a7b74dc14d59af02fb704c67685a4
hoiregeno <hoiregeno75@gmail.com>
2025-05-02 13:19:00 +1000

    Add basic markup for the digital clock

--- digital-clock/src/component/DigitalClock.jsx
+++ digital-clock/src/component/DigitalClock.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function DigitalClock() {
-  return <div>DigitalClock</div>;
+  return (
+    <div className="clock-container">
+      <h1>Digital Clock</h1>
+      <div className="clock">
+        <span>00:00:00</span>
+      </div>
+    </div>
+  );
 }
 
 export default DigitalClock;
