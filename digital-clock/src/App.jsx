89a68044720dd68663ccfcb997e20c794fd939f7
hoiregeno <hoiregeno75@gmail.com>
2025-05-02 12:58:05 +1000

    First commit: Add set up for Digital Clock program

---
+++ digital-clock/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DigitalClock from "./component/DigitalClock.jsx";
+
+function App() {
+  return <DigitalClock />;
+}
+
+export default App;
---
+++ digital-clock/src/component/DigitalClock.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DigitalClock() {
+  return <div>DigitalClock</div>;
+}
+
+export default DigitalClock;
