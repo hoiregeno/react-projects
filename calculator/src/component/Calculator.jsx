668e2dc90241b7b569d5f00a568ddb12c78b1737
hoiregeno <hoiregeno75@gmail.com>
2025-05-12 10:37:32 +1000

    feat: implement basic calculator UI with state & button handlers

--- calculator/src/component/Calculator.jsx
+++ calculator/src/component/Calculator.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Calculator() {
-  return <div>Calculator</div>;
+  //state variable to display value
+  const [displayValue, setDisplayValue] = useState("");
+  const keys = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
+  const operators = ["+", "-", "*", "/"];
+
+  // arrow function to append, clear and compute.
+  const handleButtonClick = (key) => {
+    // if the key passed in is "=", compute.
+    if (key === "=") {
+      try {
+        const result = eval(displayValue);
+        setDisplayValue(String(result));
+      } catch (error) {
+        console.error(error);
+        setDisplayValue("Error");
+      }
+    } else if (key === "C") {
+      setDisplayValue(""); // if "C", clear.
+    } else {
+      setDisplayValue((v) => v + key); // otherwise append
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <input type="text" value={displayValue} readOnly />
+      <div className="keys-container">
+        {keys.map((k) => (
+          <Button key={k} buttonKey={k} onClick={handleButtonClick} />
+        ))}
+
+        {operators.map((op) => (
+          <Button key={op} buttonKey={op} onClick={handleButtonClick} />
+        ))}
+
+        <Button buttonKey="=" onClick={handleButtonClick} />
+        <Button buttonKey="C" onClick={handleButtonClick} />
+      </div>
+    </div>
+  );
+}
+
+function Button({ buttonKey, onClick }) {
+  return <button onClick={() => onClick(buttonKey)}>{buttonKey}</button>;
 }
 
 export default Calculator;
