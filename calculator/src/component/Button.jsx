05774a89ca07de2fc1a967b4be9489ff2c611d43
hoiregeno <hoiregeno75@gmail.com>
2025-05-13 16:45:58 +1000

    first commit: add basic Button component with props

---
+++ calculator/src/component/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button({ onClick, label, className = "" }) {
+  return (
+    <button onClick={onClick} className={className}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
